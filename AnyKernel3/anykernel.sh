287e68d2677feeff8acc9139c7048a1e6d8ed568
COSMIC <rayavanindra@gmail.com>
2023-09-09 02:28:51 +0530

    Change build script and import AnyKernel3

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,46 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Velvet Kernel by TxExcalibur
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=miatoll
+device.name2=excalibur
+device.name3=curtana
+device.name4=gram
+device.name5=joyeuse
+supported.versions=11.0-13.0
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel boot install
+split_boot;
+
+flash_boot;
+flash_dtbo;
+## end boot install
+
--- build.sh
+++ build.sh
@@ -1,259 +1,91 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
- #
- # Script For Building Android Kernel
- #
+#set -e
 
-##----------------------------------------------------------##
-# Specify Kernel Directory
-KERNEL_DIR="$(pwd)"
+## Copy this script inside the kernel directory
+KERNEL_DEFCONFIG=vendor/miatoll-perf_defconfig
+ANYKERNEL3_DIR=$PWD/AnyKernel3/
+FINAL_KERNEL_ZIP=Velvet-Kernel-KSU-miatoll-$(date '+%Y%m%d').zip
+export PATH="$HOME/cosmic/bin:$PATH"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=cosmos
+export KBUILD_BUILD_USER=cosmic
+export KBUILD_COMPILER_STRING="$($HOME/cosmic/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-##----------------------------------------------------------##
-# Kernel Version Code
-VERSION=r2
+if ! [ -d "$HOME/cosmic" ]; then
+echo "Clang not found! Cloning..."
+if ! git clone -q https://gitlab.com/GhostMaster69-dev/cosmic-clang --depth=1 --single-branch ~/cosmic; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
 
-##----------------------------------------------------------##
-# Device Name and Model
-MODEL=Xiaomi
-DEVICE=MiAtoll
+# Speed up build process
+MAKE="./makeparallel"
 
-# Kernel Defconfig
-DEFCONFIG=vendor/miatoll-perf_defconfig
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
 
-# Files
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
-DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
-DTB=$(pwd)/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb
+# Clean build always lol
+echo "**** Cleaning ****"
+mkdir -p out
+make O=out clean
+make mrproper
 
-# Verbose Build
-VERBOSE=0
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+make $KERNEL_DEFCONFIG O=out
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+	       			  CC=clang \
+	          		  CROSS_COMPILE=aarch64-linux-gnu- \
+	       			  CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+	       			  LD=ld.lld \
+	       			  AR=llvm-ar \
+	       			  NM=llvm-nm \
+	       			  OBJCOPY=llvm-objcopy \
+	       			  OBJDUMP=llvm-objdump \
+	       			  STRIP=llvm-strip
 
-# Kernel Version
-KERVER=$(make kernelversion)
+echo "**** Verify Image.gz, dtbo.img & dtb ****"
+ls $PWD/out/arch/arm64/boot/Image.gz
+ls $PWD/out/arch/arm64/boot/dtbo.img
+ls $PWD/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb
 
-COMMIT_HEAD=$(git log --oneline -1)
+# Anykernel 3 time!!
+echo "**** Verifying AnyKernel3 Directory ****"
+ls $ANYKERNEL3_DIR
+echo "**** Removing leftovers ****"
+rm -rf $ANYKERNEL3_DIR/Image.gz
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/dtb
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
 
-# Date and Time
-DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
-TANGGAL=$(date +"%F%S")
+echo "**** Copying Image.gz , dtbo.img & dtb ****"
+cp $PWD/out/arch/arm64/boot/Image.gz $ANYKERNEL3_DIR/
+cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
+cp $PWD/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb $ANYKERNEL3_DIR/dtb
 
-# Specify Final Zip Name
-ZIPNAME=VelvetKernel
-FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
+echo "**** Time to zip up! ****"
+cd $ANYKERNEL3_DIR/
+zip -r9 "../$FINAL_KERNEL_ZIP" * -x README $FINAL_KERNEL_ZIP
 
-##----------------------------------------------------------##
-# Specify compiler.
+echo "**** Done, here is your sha1 ****"
+cd ..
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+rm -rf $ANYKERNEL3_DIR/Image.gz
+rm -rf $ANYKERNEL3_DIR/dtbo.img
+rm -rf $ANYKERNEL3_DIR/dtb
+rm -rf out/
 
-COMPILER=cosmic
+sha1sum $FINAL_KERNEL_ZIP
 
-##----------------------------------------------------------##
-# Specify Linker
-
-LINKER=ld.lld
-
-##----------------------------------------------------------##
-# Clone ToolChain
-function cloneTC() {
-
-	if [ $COMPILER = "atomx" ];
-	then
-        post_msg " Cloning Atomx Clang ToolChain "
-	git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
-
-	elif [ $COMPILER = "azure" ];
-	then
-        post_msg " Cloning Azure Clang ToolChain "
-	git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
-
-	elif [ $COMPILER = "cosmic" ];
-	then
-	post_msg " Cloning Cosmic Clang ToolChain "
-	git clone --depth=1 https://gitlab.com/GhostMaster69-dev/cosmic-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
-
-	elif [ $COMPILER = "proton" ];
-	then
-	post_msg " Cloning Proton Clang ToolChain "
-	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
-
-	elif [ $COMPILER = "silont" ];
-	then
-	post_msg " Cloning SilonT Clang ToolChain "
-	git clone --depth=1 https://github.com/silont-project/silont-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
-
-	elif [ $COMPILER = "aosp" ];
-	then
-        post_msg " Cloning Aosp Clang 17.0.3 ToolChain "
-        mkdir aosp-clang
-        cd aosp-clang || exit
-	wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r498229.tar.gz
-        tar -xf clang*
-        cd .. || exit
-	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
-	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
-	PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-	fi
-        # Clone AnyKernel
-        git clone --depth=1 https://github.com/txexcalibur/AnyKernel3 -b master
-
-        }
-
-##------------------------------------------------------##
-# Export Variables
-function exports() {
-
-        # Export KBUILD_COMPILER_STRING
-        if [ -d ${KERNEL_DIR}/clang ];
-           then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-        elif [ -d ${KERNEL_DIR}/gcc64 ];
-           then
-               export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)
-        elif [ -d ${KERNEL_DIR}/aosp-clang ];
-            then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-        fi
-
-        # Export ARCH and SUBARCH
-        export ARCH=arm64
-        export SUBARCH=arm64
-
-        # KBUILD HOST and USER
-        export KBUILD_BUILD_HOST=Github-CI
-        export KBUILD_BUILD_USER="TxExcalibur"
-
-        # CI
-        if [ "$CI" ]
-           then
-
-           if [ "$CIRCLECI" ]
-              then                  
-                  export CI_BRANCH=${CIRCLE_BRANCH}
-           elif [ "$DRONE" ]
-	      then		  
-		  export CI_BRANCH=${DRONE_BRANCH}
-              fi
-
-        fi
-	export PROCS=$(nproc --all)
-	export DISTRO=$(source /etc/os-release && echo "${NAME}")
-	}
-
-##----------------------------------------------------------------##
-# Telegram Bot Integration
-
-function post_msg() {
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-	-d chat_id="$chat_id" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-	}
-
-function push() {
-	curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
-	-F chat_id="$chat_id" \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2"
-	}
-##----------------------------------------------------------------##
-# Compilation
-function compile() {
-START=$(date +"%s")
-	# Push Notification
-        post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Version : </b><code>$VERSION</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
-	# Compile
-	make O=out ARCH=arm64 ${DEFCONFIG}
-	if [ -d ${KERNEL_DIR}/clang ];
-	   then
-	       make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CC=clang \
-	       HOSTCC=clang \
-	       HOSTCXX=clang++ \
-	       CROSS_COMPILE=aarch64-linux-gnu- \
-	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-	       LD=${LINKER} \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       READELF=llvm-readelf \
-	       OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-	elif [ -d ${KERNEL_DIR}/gcc64 ];
-	   then
-	       make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CROSS_COMPILE_ARM32=arm-eabi- \
-	       CROSS_COMPILE=aarch64-elf- \
-	       LD=aarch64-elf-${LINKER} \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-        elif [ -d ${KERNEL_DIR}/aosp-clang ];
-           then
-               make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CC=clang \
-               HOSTCC=clang \
-	       HOSTCXX=clang++ \
-	       CROSS_COMPILE=aarch64-linux-android- \
-	       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-               LD=${LINKER} \
-               AR=llvm-ar \
-               NM=llvm-nm \
-               OBJCOPY=llvm-objcopy \
-               OBJDUMP=llvm-objdump \
-               STRIP=llvm-strip \
-               READELF=llvm-readelf \
-               OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-        fi
-
-	# Verify Files
-	if ! [ -a "$IMAGE" ];
-	   then
-	       push "error.log" "Build Throws Errors"
-	       exit 1
-	   else
-      	       post_msg " Kernel Compilation Finished. Started Zipping "
-	fi
-	}
-
-##----------------------------------------------------------------##
-function zipping() {
-	# Copy Files To AnyKernel3 Zip
-	cp $IMAGE AnyKernel3
-	cp $DTBO AnyKernel3
-	cp $DTB AnyKernel3/dtb
-
-	# Zipping and Push Kernel
-	cd AnyKernel3 || exit 1
-        zip -r9 ${FINAL_ZIP} *
-        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-        push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-        cd ..
-        }
-
-##----------------------------------------------------------##
-
-cloneTC
-exports
-configs
-compile
-END=$(date +"%s")
-DIFF=$(($END - $START))
-zipping
-
-##----------------*****-----------------------------##
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
